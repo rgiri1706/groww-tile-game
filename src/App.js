d26f78e8294dfc92c6f02ae47723b1eb32a15c7e
Rohit Giri <rohit.girithoughtworks.com@Rohits-MacBook-Pro.local>
2021-05-28 12:32:51 +0530

    Tile game assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const arr = [1,2,3,4,5,6,7,8,9];
+  const [chances, updateChances] = useState(3);
+  const [ inputNumber, setInput ] = useState('');
+  const click =(num)=> {
+    if(parseInt(inputNumber) === num){
+      alert('Congrats you win');
+      updateChances(3);
+      setInput('');
+    }else if(chances === 1){
+      alert('Sorry, you loose');
+      updateChances(3);
+      setInput('');
+    }else{
+      updateChances(chances-1);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+          <div className="input-style">
+            <label className="mg-bt-16">Enter a number between 1 to 9</label>
+            <input type="number" min="0" max="9" value={inputNumber} onChange={(event)=> setInput(event.target.value)} className="mg-bt-16" />
+          </div>
+          {inputNumber > 0 && inputNumber< 10 && <div className="tile-style">
+            {
+              arr.map((ele, index)=><button className="tile-element" key ={index} onClick={()=>click(ele)}>Find</button>)
+            }
+          </div>}
+          {inputNumber>0 && inputNumber< 10 && <div className="chances-label">
+            chances: {chances}
+          </div>}
+      </div>
     </div>
   );
 }
